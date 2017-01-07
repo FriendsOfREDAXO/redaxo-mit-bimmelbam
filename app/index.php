2dbfdbf1c75aa74c503557c7e32cf3574aa63d08
schuer <ds@decaf.de>
2017-01-07 13:31:17 +0100

    add hints to frontend demo

--- app/index.php
+++ app/index.php
@@ -2,4 +2,6 @@
 
     An dieser Stelle wäre die fertig implementierte REDAXO-Website zu sehen.
 
+    Den Frontend-Prototyp findest du hier: <a href="/demo">/demo</a>
+    
 </pre>
--- app/redaxo/index.php
+++ app/redaxo/index.php
@@ -2,4 +2,6 @@
 
     An dieser Stelle wäre der Admin-Bereich von REDAXO zu sehen.
 
+    Den Frontend-Prototyp findest du hier: <a href="/demo">/demo</a>
+    
 </pre>
