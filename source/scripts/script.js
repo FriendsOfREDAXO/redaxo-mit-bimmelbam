0bcd9688262c25d7c92c5b2a70a25c4dbd0684b5
Hirbod Mirjavadi <hirbod@hirbod.local>
2019-10-19 17:52:29 +0200

    remove tiny slider

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -3,7 +3,6 @@ import $ from 'jquery';
 import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/carousel';
 import 'bootstrap/js/dist/tooltip';
-import { tns } from "tiny-slider/src/tiny-slider"
 
 // import demo module
 import fooFighters from './demo-modules/foo-fighters';
