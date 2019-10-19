ec34913279612b18c247b80572d17bc6392ed824
Hirbod Mirjavadi <hirbod@hirbod.local>
2019-10-19 18:01:56 +0200

    comments

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,5 +1,9 @@
-// import components
+// import jquery
 import $ from 'jquery';
+
+// import bootstrap components from node_modules folder	import 'bootstrap/js/dist/carousel';
+// see https://getbootstrap.com/docs/4.1/getting-started/javascript/
+import 'popper.js';
 import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/carousel';
 import 'bootstrap/js/dist/tooltip';
